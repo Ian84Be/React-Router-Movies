d0905705920e5a6cec2ef0876d518d3d3418c66e
Ian Be <ian84be@gmail.com>
2019-02-26 16:42:19 -0600

    stretch:refactor MovieCard

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default class Movie extends Component {
   constructor(props) {
     super(props);
@@ -10,10 +12,7 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = this.props.match.params.id;
-    console.log(id);
-    this.fetchMovie(id);
+    this.fetchMovie(this.props.match.params.id);
   }
 
   fetchMovie = id => {
@@ -22,9 +21,7 @@ export default class Movie extends Component {
       .then(response => {
         this.setState(() => ({ movie: response.data }));
       })
-      .catch(error => {
-        console.error(error);
-      });
+      .catch(error => console.error(error));
   };
   // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
@@ -39,31 +36,7 @@ export default class Movie extends Component {
   // }
 
   render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
-
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className="save-button">Save</div>
-      </div>
-    );
+    if (!this.state.movie) return <div>Loading movie information...</div>;
+    return <MovieCard movie={this.state.movie} />
   }
 }
